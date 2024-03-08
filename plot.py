22044bc9139d81f408f381bf5906cf68448afac0
Jan Steggemann <jan.steggemann@cern.ch>
2024-03-08 11:12:26 +0100

    Initial import

---
+++ plot.py
@@ -0,0 +1,86 @@
+import uproot
+import numpy as np
+import awkward as ak
+from scipy import stats
+import matplotlib.pyplot as plt
+import mplhep as hep
+
+hep.style.use("CMS")
+
+
+def make_plot(sample):
+    fig = plt.figure(num=1, clear=True)
+    ax = fig.add_subplot()
+
+    hep.cms.label('', data=False, lumi=None, year=2018)
+
+    f = uproot.open(f'/Users/jan/cernbox/{sample}.root')
+    t = f['Events']
+
+    # Make a plot of the missing ET resolution
+
+
+    # Get the missing ET
+    genmet = t['GenMET_pt'].array()
+    genmetphi = t['GenMET_phi'].array()
+
+
+    algos = ['MET', 'PuppiMET', 'DeepMETResolutionTune']
+
+    # for algo in algos:
+    #     met = t[algo+'_pt'].array()
+    #     res = (met - genmet)
+
+    #     print(res, type(res))
+
+    #     # Plotting the histogram as a line
+    #     plt.hist(res,  bins=10, range=(-100, 100), density=True, histtype='step', label=algo) #, color='blue')
+
+
+    # # Make a plot
+    # plt.xlabel('MET resolution [GeV]')
+    # plt.ylabel('Events')
+    # plt.legend()
+    # plt.show()
+
+    # Make a plot of the standard deviation of the MET resolution as a function of the LHE HT
+    label = 'LHE HT'
+
+    if sample in ['SMS-T5qqqqHg', 'GJet']:
+        ht = ak.max(t['Photon_pt'].array(), axis=1)
+        ht = ak.fill_none(ht, 0)
+        label = 'Leading photon p$_T$'
+    elif sample in ['QCD']:
+        ht = ak.max(t['Jet_pt'].array(), axis=1)
+        ht = ak.fill_none(ht, 0)
+        label = 'Leading jet p$_T$'
+    else:
+        ht = ak.sum(t['GenJet_pt'].array(), axis=1)
+        label = 'H$_T$ (generator jets)'
+    # ht_bins = np.linspace(0, 3000, 15)
+    # ht_bins = np.histogram_bin_edges(ht, bins=15)
+
+    ht_bins = stats.mstats.mquantiles(ht, np.linspace(0, 1, 15))
+    ht_bins = ht_bins[:-1]
+    ht_centers = (ht_bins[1:] + ht_bins[:-1]) / 2
+
+    for algo in algos:
+        met = t[algo+'_pt'].array()
+        res = (met - genmet)
+
+        std = np.zeros(len(ht_centers))
+        for i in range(len(ht_centers)):
+            std[i] = np.std(res[(ht > ht_bins[i]) & (ht < ht_bins[i+1])])
+        plt.errorbar(ht_centers, std, xerr=(ht_bins[1:] - ht_bins[:-1]) / 2, fmt='o', label=algo)
+    plt.xlabel(f'{label} [GeV]')
+    plt.ylabel('p$_T^{miss}$ resolution [GeV]')
+    plt.legend()
+    fig.savefig(f"{sample}.pdf", format="pdf")
+    fig.savefig(f"{sample}.png", format="png")
+    # plt.show()
+
+samples = ['GJet', 'HINV', 'TTTT', 'QCD', 'SMS-T5qqqqHg', 'SMS-T2tt-4bd', 'SMS-TChiZZ']
+if __name__ == "__main__":
+    for sample in samples:
+        make_plot(sample)
+
